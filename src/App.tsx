1f70b1b942f663354453c3602d8431bdb52fb725
mehrnaz-MNH <mnh.mehrnaz@gmail.com>
2024-09-16 14:58:52 +1000

    todo works , maybe later I add styling and edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,57 @@
-import React, { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Head } from './Head'
-import { Input } from './Input'
-import { Todo } from './model'
+import React, { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Head } from "./Head";
+import { Input } from "./Input";
+import { Todo } from "./model";
+import TodoList from "./TodoList";
 
-const App : React.FC = () => {
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-   const [todo , setTodo] = useState<string>("")
-   const [todos , setTodos] = useState<Todo[]>([])
-
-   const handleAdd = (e: React.FormEvent) => {
-    e.preventDefault()
-    if(todo){
-      setTodos([...todos , {
-        id: Date.now(),
-        task : todo ,
-        isDone: false,
-      }])
-      setTodo("")
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([
+        ...todos,
+        {
+          id: Date.now(),
+          task: todo,
+          isDone: false,
+        },
+      ]);
+      setTodo("");
     }
+  };
 
-   }
-
-   console.log(todos)
+  console.log(todos);
 
   return (
- <div>
-      <Head/>
-      <Input todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
- </div>
-  )
-}
+    <div>
+      <Head />
+      <Input todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <h1> To Do Task </h1>
+      <TodoList todos={todos} setTodos={setTodos} />
 
-export default App
+      <h1>Done Task</h1>
+
+      <div>
+        <ul>
+          {todos.map((todo) =>
+            todo.isDone ? (
+              <>
+                <li key={todo.id}>
+                  <span>{todo.task}</span>
+                </li>
+              </>
+            ) : null
+          )}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,19 +1,55 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Todo } from "./model";
 
 interface Props {
-
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-const TodoList = ({}) => {
+const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
+  const [task, setTask] = useState<string>("");
+
+  const handleDone = (id: number) => {
+    const updatedList: Todo[] = todos.map((todo) =>
+      todo.id === id ? { ...todo, isDone: true } : todo
+    );
+
+    setTodos(updatedList);
+  };
+
+  const handleDelete = (id: number) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+
+    setTodos(newTodos);
+  };
+
+  const handleEdit = (id: number, TextValue: string) => {
+    const updatedList: Todo[] = todos.map((todo) =>
+      todo.id === id ? { ...todo, task: TextValue } : todo
+    );
+
+    setTodos(updatedList);
+  };
+
   return (
     <div>
-        <ul>
-            <l>
+      <ul>
+        {todos.map((todo) =>
+          !todo.isDone ? (
+            <>
+              <li key={todo.id}>
+                <span> {todo.task} </span>
 
-            </l
-        </ul>
+                <button>edit</button>
+                <button onClick={() => handleDelete(todo.id)}>delete</button>
+                <button onClick={() => handleDone(todo.id)}>check done</button>
+              </li>
+            </>
+          ) : null
+        )}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default TodoList
+export default TodoList;
